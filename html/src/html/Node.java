205126c941f2d04cd65fa01b7e2589b5905a4157
thoma <thoma@10.46.254.112>
2020-02-28 18:01:39 +0100

    Array based implementation

--- html/src/html/Node.java
+++ html/src/html/Node.java
@@ -1,47 +1,69 @@
 package html;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @invar | (getTag() == null) != (getText() == null)
+ * @invar | getParent() == null || Arrays.stream(getParent().getChildren()anyMatch(c -> c == this)
+ * @invar | Arrays.stream(getChildren()
+ */
 public class Node {
+	/**
+	 * @invar Either {@code tag} or {@code text} must be non-null.
+	 * 		| tag != null || text != null
+	 * @invar Either {@code tag} or {@code text} must be null.
+	 * 		| tag == null || text == null
+	 * @invar 
+	 * 		| (tag == null) == (text != null)
+	 * @invar 
+	 * 		| children != null
+	 * @invar If this node has a parent, this node is among its parent's children. 
+	 * 		| parent == null || Array.stream(parent.children).anyMatch(c -> c == this)
+	 * @invar For each child of this node, the child's parent must equal this node.
+	 * 		| Arrays.stream(children).allMatch(c -> c != null && c.parent == this)
+	 */
 	private String tag;
 	private String text;
-	private Node firstChild;
-	private Node lastChild;
-	private Node nextSibling;
-	private Node previousSibling;
 	private Node parent;
+	private Node[] children;
+	
+	public String getTag() { return tag; }
+	public String getText() { return text; }
+	
+	public Node getParent() { return parent; }
+	
+	public Node[] getChildren() {
+		//return children; FOUT: REPTRESENTATION EXPOSURE
+		return Arrays.copyOf(children, children.length);
+	}
 	
 	public Node(String tag, String text) {
 		this.tag = tag;
 		this.text = text;
+		this.children = new Node[0];
 	}
 	
 	public void addChild(Node child) {
-		if (lastChild == null) {
-			firstChild = child;
-			lastChild = child;
-		}
-		else {
-			lastChild.nextSibling = child;
-			child.previousSibling = lastChild;
-			lastChild = child;
-		}
+		Node[] newChildren = new Node[children.length + 1];
+		System.arraycopy(children, 0, newChildren, 0, children.length);
+		newChildren[children.length] = child;
+		children = newChildren;
 		child.parent = this;
 	}
 	
 	public void removeChild(Node child) {
-		if (firstChild == child) {
-			firstChild = firstChild.nextSibling;
-			firstChild.previousSibling = null;
-			if (firstChild == null)
-				lastChild = null;
-		}
-		else if (child.nextSibling != null) 
-			child.previousSibling.nextSibling = child.nextSibling; 
-		else {
-			lastChild = child.previousSibling;
-			lastChild.nextSibling = null;
-		}
-		child.previousSibling = null;
-		child.nextSibling = null;
+		Node[] newChildren = new Node[children.length - 1];
+		int index = 0;
+		while (children[index] != child)
+			index++;
+		System.arraycopy(children, 0, newChildren, 0, index);
+		System.arraycopy(children, index + 1, newChildren, index, children.length - index - 1);
+		children = newChildren;
 		child.parent = null;
 	}
 	
@@ -54,10 +76,10 @@ public class Node {
 		if (text != null)
 			return text;
 		String result = "<" + tag + ">";
-		Node child = firstChild;
-		while (child != null) {
+		for (Node child : children) {
+		// for (int = 0; i< children.length; i++)
+			//Node
 			result += child.toString();
-			child = child.nextSibling;
 		}
 		result += "</" + tag + ">";
 		return result;
